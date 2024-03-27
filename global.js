132b8038609ec8a5db952a2543096cc69dc21817
Danilo Pacelli <152805009+pacellii@users.noreply.github.com>
2024-03-27 11:46:32 -0300

    update index.html

---
+++ global.js
@@ -0,0 +1,23 @@
+const body = document.body
+const footer = document.createElement('footer');
+footer.setAttribute('id', 'footer');
+
+footer.setAttribute('class', 'footer');
+
+body.appendChild(footer);
+
+const imgFooter = 
+        
+        "<img src='./assets/img/logo.png' alt='Logo footer' class='imgFooter'>"+
+        "<p>Todos os direitos reservadors</p>";
+       
+
+    footer.innerHTML += imgFooter;
+
+
+
+
+            
+
+    
+
